74876420abe8c937f58128116188e9d3c022e9fb
Joshua Zhang <toruta39@gmail.com>
2016-09-10 19:41:43 +0900

    add native alert for clear completed items confirmation

--- js/components/WebAppShell.js
+++ js/components/WebAppShell.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {
   Text,
-  View
+  View,
+  Alert
 } from 'react-native'
 import WebViewBridge from 'react-native-webview-bridge'
 
@@ -9,7 +10,24 @@ export default class WebAppShell extends Component {
   render() {
     return (
       <View style={{alignSelf: 'stretch', backgroundColor: '#fff', flex: 1}}>
-        <WebViewBridge source={{uri: "http://www.google.com"}} />
+        <WebViewBridge ref="webviewbridge"
+                       onBridgeMessage={(message) => {
+                         if (message === 'CLEAR_COMPLETED') {
+                           Alert.alert(
+                             'Heads Up!',
+                             'Do you really want to clear all completed items?', [
+                                {
+                                  text: 'Yes, I\'m sure.',
+                                  onPress: () => this.refs.webviewbridge.sendToBridge('CLEAR_COMPLETED_SUCCESS'),
+                                  style: 'destructive'
+                                },
+                                {
+                                  text: 'No, I don\'t think so.'
+                                }
+                             ])
+                         }
+                       }}
+                       source={{uri: "http://localhost:3000"}} />
       </View>
     )
   }
