d1af6032c29337e91ec314b73ae531b6775041e8
Joshua Zhang <toruta39@gmail.com>
2016-09-11 17:22:22 +0900

    refactor WebAppShell for readability

--- js/components/WebAppShell.js
+++ js/components/WebAppShell.js
@@ -7,26 +7,28 @@ import {
 import WebViewBridge from 'react-native-webview-bridge'
 
 export default class WebAppShell extends Component {
+  onBridgeMessage(message) {
+    if (message === 'CLEAR_COMPLETED') {
+      Alert.alert(
+        'Heads Up!',
+        'Do you really want to clear all completed items?', [
+           {
+             text: 'Yes, I\'m sure.',
+             onPress: () => this.refs.webviewbridge.sendToBridge('CLEAR_COMPLETED_SUCCESS'),
+             style: 'destructive'
+           },
+           {
+             text: 'No, I don\'t think so.'
+           }
+        ])
+    }
+  }
+
   render() {
     return (
       <View style={{alignSelf: 'stretch', backgroundColor: '#fff', flex: 1}}>
         <WebViewBridge ref="webviewbridge"
-                       onBridgeMessage={(message) => {
-                         if (message === 'CLEAR_COMPLETED') {
-                           Alert.alert(
-                             'Heads Up!',
-                             'Do you really want to clear all completed items?', [
-                                {
-                                  text: 'Yes, I\'m sure.',
-                                  onPress: () => this.refs.webviewbridge.sendToBridge('CLEAR_COMPLETED_SUCCESS'),
-                                  style: 'destructive'
-                                },
-                                {
-                                  text: 'No, I don\'t think so.'
-                                }
-                             ])
-                         }
-                       }}
+                       onBridgeMessage={this.onBridgeMessage.bind(this)}
                        source={{uri: "http://localhost:3000"}} />
       </View>
     )
