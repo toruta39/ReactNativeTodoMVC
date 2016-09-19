c086a324b2582abbfdbbfdda1f276b49dfa6e28d
Joshua Zhang <toruta39@gmail.com>
2016-09-19 21:09:55 +0900

    workaround webpack issue https://github.com/webpack/webpack/issues/1082

--- web/webpack.config.dev.js
+++ web/webpack.config.dev.js
@@ -32,7 +32,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin()
   ],
   resolve: {
